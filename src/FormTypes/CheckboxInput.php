214deeb2bce9cbf7f974d2e72ee82435a1eddde4
Rignchen <rinchendemort@gmail.com>
2024-02-08 10:23:02 +0100

    feat: implement checkboxes

---
+++ src/FormTypes/CheckboxInput.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Rignchen\Forms\FormTypes;
+use Rignchen\Forms\FormType;
+
+class CheckboxInput implements FormType {
+
+    public function __construct(
+        private readonly string $name,
+        private readonly string $text,
+        private readonly string $class = '',
+        private $callable = null
+    ) {}
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getCallable(): callable {
+        return $this->callable ?? fn($value) => $value;
+    }
+    public function call($value, callable $callable) {
+        if ($this->callable) {
+            return $callable($value);
+        }
+    }
+
+    public function render($value): string {
+        $checked = $value===null ? '' : 'checked';
+        return "<input type='checkbox' name='$this->name' class='$this->class' id='$this->name' value='' $checked><label for='$this->name'>$this->text</label>";
+    }
+}
