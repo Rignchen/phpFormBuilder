9f81525cf8773097d2b942dc6c8c0c23989827e7
Rignchen <rinchendemort@gmail.com>
2024-02-08 08:06:45 +0100

    feat: store field list

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -6,10 +6,21 @@ class FormBuilder {
     private string $action;
     private string $method;
     private string $class;
+    private array $fields = [];
     public function __construct(string $action = 'get', string $method = '', string $class = '') {
+        if (!in_array($action, ['get', 'post'])) {
+            throw new \InvalidArgumentException('Invalid action');
+        }
         $this->action = $action;
         $this->method = $method;
         $this->class = $class;
+    }
+
+    public function addField(FormType $type) {
+        $temp = $type;
+        $name = $temp->getName();
+        $this->fields[$name] = $temp;
+        return $this->try_call($name, $temp);
     }
     }
 
