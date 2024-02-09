f1f1e9dd7bfe08f7d50d87803b523e7732ce80b2
Rignchen <rinchendemort@gmail.com>
2024-02-09 20:10:23 +0100

    feat: implement text area

---
+++ src/FormTypes/TextArea.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rignchen\Forms\FormTypes;
+use Rignchen\Forms\FormType;
+
+class TextArea implements FormType {
+    private string $id;
+    public function __construct(
+        private readonly string $name,
+        private readonly string $class = '',
+        private $callable = null,
+        private readonly string $value = '',
+        private readonly int|null $cols = null,
+        private readonly int|null $rows = null
+    ) {}
+
+    public function getName(): string {
+        return $this->name;
+    }
+    public function getID(): string {
+        return $this->id;
+    }
+    public function setID(string $id): void {
+        $this->id = $id;
+    }
+
+    public function getCallable(): callable {
+        return $this->callable ?? fn($value) => $value;
+    }
+    public function call($value, callable $callable): void {
+        $callable($value);
+    }
+
+    public function render($value): string {
+        if ($value === null) $value = $this->value;
+        return "<textarea name='$this->name' class='$this->class' id='$this->id'"
+            . ($this->cols !== null ? " cols='$this->cols'" : '')
+            . ($this->rows !== null ? " rows='$this->rows'" : '')
+            . ">$value</textarea>";
+    }
+}
